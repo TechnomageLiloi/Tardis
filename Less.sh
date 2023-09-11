0731681bcf389bf969ac7cec28920da8391b3e68
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-09-11 15:50:02 +0300

    [0.2.9] Add private example.

--- Less.sh
+++ Less.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Compile all I60 LESS to CSS
+
 lessc ./Engine/Style.less ./Engine/Style.css
 lessc ./Engine/API/Application/Diary/Edit/Style.less ./Engine/API/Application/Diary/Edit/Style.css
 lessc ./Engine/API/Application/Diary/Show/Style.less ./Engine/API/Application/Diary/Show/Style.css
