661e94e42d400de21a81682ef5f62582084d6041
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-09-13 20:06:54 +0300

    [0.3.8] Add exception log to database.

--- index.php
+++ index.php
@@ -5,5 +5,48 @@ define('ROOT_DIR', __DIR__);
 session_start();
 include_once __DIR__ . '/vendor/autoload.php';
 
-$config = include __DIR__ . '/Config/Block.php';
-echo (new Liloi\I60\Application($config))->compile();
+use Liloi\Judex\Log;
+use Liloi\Judex\LogData;
+use Liloi\I60\Domain\Logs\Entity;
+use Liloi\Judex\Exceptions\JudexException;
+
+Log::set('log', function (LogData $data) {
+    $entity = Entity::create([
+        'title' => $data->getMessage(),
+        'data' => json_encode($data->getData(), JSON_UNESCAPED_UNICODE)
+    ]);
+    $entity->insert();
+});
+
+try {
+    $config = include __DIR__ . '/Config/Block.php';
+    echo (new Liloi\I60\Application($config))->compile();
+}
+catch (JudexException $exp)
+{
+    Log::call(
+        $exp->getMessage(),
+        'judex exception',
+        [
+            'class' => get_class($exp),
+            'trace' => $exp->getTrace(),
+            'code' => $exp->getCode(),
+            'file' => $exp->getFile(),
+            'line' => $exp->getLine()
+        ]
+    );
+}
+catch (Throwable $exp)
+{
+    Log::call(
+        $exp->getMessage(),
+        'not judex exception',
+        [
+            'class' => get_class($exp),
+            'trace' => $exp->getTrace(),
+            'code' => $exp->getCode(),
+            'file' => $exp->getFile(),
+            'line' => $exp->getLine()
+        ]
+    );
+}
