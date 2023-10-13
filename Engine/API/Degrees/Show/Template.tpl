0b8b3472f1f656644d8f6bc266ba94295779fa57
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-10-13 07:21:23 +0300

    [0.4.11] Resort problem types.

--- Engine/API/Degrees/Show/Template.tpl
+++ Engine/API/Degrees/Show/Template.tpl
@@ -2,8 +2,7 @@
     <h1><?php echo $entity->getTitle(); ?></h1>
     <hr/>
     <a href="javascript:void(0)" onclick="Tardis.Degrees.edit('protos')">Edit</a> &diams;
-    <a href="javascript:void(0)" onclick="Tardis.Problems.collection('protos')">Problems list</a> &diams;
-    <a href="javascript:void(0)" onclick="Tardis.Lessons.schedule('<?php echo gmdate('Y-m-d'); ?>')">Schedule</a>
+    <a href="javascript:void(0)" onclick="Tardis.Problems.collection('protos')">Problems list</a>
     <hr/>
     <?php echo $entity->getParse(); ?>
 </div>
--- Engine/Domain/Problems/Types.php
+++ Engine/Domain/Problems/Types.php
@@ -4,20 +4,20 @@ namespace Liloi\Tardis\Domain\Problems;
 
 class Types
 {
-    public const PROTOCOLS = 1;
-    public const TRAINING = 2;
-    public const PROJECTS = 3;
-    public const SURVIVAL = 4;
-    public const EXAMS = 5;
+    public const ADVENTURE = 1;
+    public const CODEX = 2;
+    public const BIOTECH = 3;
+    public const EXAMS = 4;
+    public const PROJECTS = 5;
     public const ARTIFACTS = 6;
     public const FAMILY = 7;
 
     public static $list = [
-        self::PROTOCOLS => 'Protocols',
-        self::TRAINING => 'Training',
-        self::PROJECTS => 'Projects',
-        self::SURVIVAL => 'Survival',
+        self::ADVENTURE => 'Adventure',
+        self::CODEX => 'Codex',
+        self::BIOTECH => 'Biotech',
         self::EXAMS => 'Exams',
+        self::PROJECTS => 'Projects',
         self::ARTIFACTS => 'Artifacts',
         self::FAMILY => 'Family'
     ];
