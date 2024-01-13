65887aad5328cc6b4153a20ae6af74357a8d369c
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-01-13 04:40:08 +0200

    [0.0.7] Add SQL source.

---
+++ Install/Database.sql
@@ -0,0 +1,30 @@
+create table tardis_degrees
+(
+    key_degree bigint unsigned auto_increment,
+    uid varchar(100) not null,
+    title varchar(250) null,
+    program mediumtext null,
+    status tinyint unsigned not null,
+    constraint tardis_degrees_pk
+        primary key (key_degree)
+);
+
+create unique index tardis_degrees_uid_uindex
+    on tardis_degrees (uid);
+
+insert into tardis_degrees(uid, title, program, status) values("protos", "Protos", "-", 1);
+
+create table tardis_lessons
+(
+    key_lesson bigint unsigned auto_increment,
+    comment varchar(250) not null,
+    mark tinyint unsigned default 0 not null,
+    status tinyint unsigned default 1 not null,
+    start timestamp not null,
+    finish timestamp not null,
+    data json not null,
+    type tinyint unsigned default 1 not null,
+    tags varchar(100) default '' not null,
+    constraint tardis_lessons_pk
+        primary key (key_lesson)
+);
