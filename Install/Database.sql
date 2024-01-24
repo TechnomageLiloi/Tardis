ac0fa1ab9f5ec9afa6bead8a8f812cd42d24eec0
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-01-24 09:51:06 +0200

    [0.3.6] Update problems SQL table.

--- Install/Database.sql
+++ Install/Database.sql
@@ -30,14 +30,14 @@ create table tardis_lessons
 create table tardis_problems
 (
     key_problem bigint unsigned auto_increment,
-    key_lesson bigint unsigned not null,
+    key_degree bigint unsigned not null,
     title varchar(250) not null,
-    start time not null,
     status tinyint unsigned default 1 not null,
+    summary text not null,
     constraint tardis_problems_pk
         primary key (key_problem),
-    constraint tardis_problems_tardis_lessons_key_lesson_fk
-        foreign key (key_lesson) references tardis_lessons (key_lesson)
+    constraint tardis_problems_tardis_degrees_key_degree_fk
+        foreign key (key_degree) references tardis_degrees (key_degree)
             on update cascade on delete cascade
 );
 
