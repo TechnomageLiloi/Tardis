09c2a3b180f9f855da40cc7e8f10a38351a5dc9a
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-02-06 11:35:23 +0200

    [0.4.0] Add NOT_IN_HAND problem status.

--- Engine/Domain/Problems/Statuses.php
+++ Engine/Domain/Problems/Statuses.php
@@ -6,12 +6,14 @@ class Statuses
 {
     public const TODO = 1;
     public const IN_HAND = 2;
+    public const NOT_IN_HAND = 5;
     public const COMPLETE = 3;
     public const ARCHIVE = 4;
 
     public static $list = [
         self::TODO => 'To Do',
         self::IN_HAND => 'In hand',
+        self::NOT_IN_HAND => 'Not in hand',
         self::COMPLETE => 'Complete',
         self::ARCHIVE => 'Archive (will be invisible)',
     ];
