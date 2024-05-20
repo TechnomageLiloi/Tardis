693981c5626eb00ef7cc1871f08096270dbc724b
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-05-20 09:58:47 +0300

    [0.4.5] Update quest types.

--- Engine/Domain/Quests/Types.php
+++ Engine/Domain/Quests/Types.php
@@ -5,18 +5,18 @@ namespace Liloi\Rune\Domain\Quests;
 class Types
 {
     public const CODEX = 1;
-    public const BIOTECH_BIONIC = 2;
-    public const BIOTECH_MENTATUS = 3;
-    public const BIOTECH_PRIORUS = 4;
+    public const BIOTECH = 2;
+    public const GAMES = 3;
+    public const ADVENTURE = 4;
     public const ARTIFACT = 5;
     public const PROBLEM = 6;
     public const FAMILY = 7;
 
     public static $list = [
         self::CODEX => 'Codex',
-        self::BIOTECH_BIONIC => 'Biotech Bionic',
-        self::BIOTECH_MENTATUS => 'Biotech Mentatus',
-        self::BIOTECH_PRIORUS => 'Biotech Priorus',
+        self::BIOTECH => 'Biotech',
+        self::GAMES => 'Games',
+        self::ADVENTURE => 'Adventure',
         self::ARTIFACT => 'Artifact',
         self::PROBLEM => 'Problem',
         self::FAMILY => 'Family',
