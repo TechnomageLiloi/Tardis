763dea4d192cfa4f10f2106fb9edcfb6b04babc2
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-05-21 15:42:07 +0300

    [0.4.8] Rename lesson types.

--- Engine/Domain/Quests/Types.php
+++ Engine/Domain/Quests/Types.php
@@ -6,8 +6,8 @@ class Types
 {
     public const CODEX = 1;
     public const BIOTECH = 2;
-    public const GAMES = 3;
-    public const ADVENTURE = 4;
+    public const MENTAT = 3;
+    public const PRIOR = 4;
     public const ARTIFACT = 5;
     public const PROBLEM = 6;
     public const FAMILY = 7;
@@ -15,8 +15,8 @@ class Types
     public static $list = [
         self::CODEX => 'Codex',
         self::BIOTECH => 'Biotech',
-        self::GAMES => 'Games',
-        self::ADVENTURE => 'Adventure',
+        self::MENTAT => 'Mentat',
+        self::PRIOR => 'Prior',
         self::ARTIFACT => 'Artifact',
         self::PROBLEM => 'Problem',
         self::FAMILY => 'Family',
