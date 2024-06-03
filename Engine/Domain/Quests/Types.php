bf58a83cdb81c0a21cffc60fd9852a616862ad27
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-06-03 06:44:35 +0300

    [0.4.9] Fix types of lessons.

--- Engine/Domain/Quests/Types.php
+++ Engine/Domain/Quests/Types.php
@@ -8,17 +8,17 @@ class Types
     public const BIOTECH = 2;
     public const MENTAT = 3;
     public const PRIOR = 4;
-    public const ARTIFACT = 5;
-    public const PROBLEM = 6;
-    public const FAMILY = 7;
+    public const SPELLS = 5;
+    public const QUESTS = 6;
+    public const HOME = 7;
 
     public static $list = [
         self::CODEX => 'Codex',
         self::BIOTECH => 'Biotech',
         self::MENTAT => 'Mentat',
         self::PRIOR => 'Prior',
-        self::ARTIFACT => 'Artifact',
-        self::PROBLEM => 'Problem',
-        self::FAMILY => 'Family',
+        self::SPELLS => 'Spells',
+        self::QUESTS => 'Quests',
+        self::HOME => 'Home',
     ];
 }
